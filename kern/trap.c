4b9c2fbe15c8ef120b8e35bd31f3a529d3279bbf
lewesche <wesche@usc.edu>
2020-09-24 15:42:42 -0700

    passing lab3 part A!

--- kern/trap.c
+++ kern/trap.c
@@ -58,6 +58,24 @@ static const char *trapname(int trapno)
 	return "(unknown trap)";
 }
 
+extern void trap0();
+extern void trap1();
+extern void trap2();
+extern void trap3();
+extern void trap4();
+extern void trap5();
+extern void trap6();
+extern void trap7();
+extern void trap8();
+extern void trap10();
+extern void trap11();
+extern void trap12();
+extern void trap13();
+extern void trap14();
+extern void trap16();
+extern void trap17();
+extern void trap18();
+extern void trap19();
 
 void
 trap_init(void)
@@ -65,6 +83,31 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
+	
+	// Gatedesc type and macros from inc/mmu.h
+	// Need handler segment, offset, and permissions
+	unsigned istrap = 1;	// istrap = 1 for trap
+	unsigned sel = GD_KT;	// segment should be the seg where the interrupt function lives - there is a macro for this, (GD_KT in memlayout.h) but this is a constant, does it change with paging? 
+	unsigned dpl = 0;		// offset might just be the function address (segment addresses are just the same as virtual addresses in jos?) - or a portion of it?
+
+	SETGATE(idt[0], istrap, sel, trap0, dpl);
+	SETGATE(idt[1], istrap, sel, trap1, dpl);
+	SETGATE(idt[2], istrap, sel, trap2, dpl);
+	SETGATE(idt[3], istrap, sel, trap3, dpl);
+	SETGATE(idt[4], istrap, sel, trap4, dpl);
+	SETGATE(idt[5], istrap, sel, trap5, dpl);
+	SETGATE(idt[6], istrap, sel, trap6, dpl);
+	SETGATE(idt[7], istrap, sel, trap7, dpl);
+	SETGATE(idt[8], istrap, sel, trap8, dpl);
+	SETGATE(idt[10], istrap, sel, trap10, dpl);
+	SETGATE(idt[11], istrap, sel, trap11, dpl);
+	SETGATE(idt[12], istrap, sel, trap12, dpl);
+	SETGATE(idt[13], istrap, sel, trap13, dpl);
+	SETGATE(idt[14], istrap, sel, trap14, dpl);
+	SETGATE(idt[16], istrap, sel, trap16, dpl);
+	SETGATE(idt[17], istrap, sel, trap17, dpl);
+	SETGATE(idt[18], istrap, sel, trap18, dpl);
+	SETGATE(idt[19], istrap, sel, trap19, dpl);
 
 	// Per-CPU setup 
 	trap_init_percpu();
