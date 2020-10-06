f22b231d6a24b1cffcb498d7bd097eca9e58d892
lewesche <wesche@usc.edu>
2020-10-05 23:05:51 -0700

    ex3 working

--- kern/pmap.c
+++ kern/pmap.c
@@ -215,7 +215,8 @@ mem_init(void)
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
 
-	boot_map_region(kern_pgdir, KSTACKTOP-KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W);
+	//Replaced in lab 4 
+	//boot_map_region(kern_pgdir, KSTACKTOP-KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -279,6 +280,10 @@ mem_init_mp(void)
 	//
 	// LAB 4: Your code here:
 
+	for(int i=0; i<NCPU; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP -i*(KSTKSIZE+KSTKGAP);
+		boot_map_region(kern_pgdir, kstacktop_i-KSTKSIZE, KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
